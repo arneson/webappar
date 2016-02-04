3016d5a8a81706c76411be3eca4abfc42772d29c
Victor Larsson <larsson.vic@gmail.com>
2016-02-04 17:01:37 +0100

    finished services.js

--- workshop2/ws_shop/src/main/webapp/shop/app/js/services.js
+++ workshop2/ws_shop/src/main/webapp/shop/app/js/services.js
@@ -8,13 +8,30 @@ var productCatalogueService = angular.module('ProductCatalogueService', []);
 productCatalogueService.factory('ProductCatalogueProxy', ['$http',
     function($http) {
 
-        var url = 'TODO';
+        var url = 'http://localhost:8080/ws_shop_war_exploded/webresources/products';
 
-  
         return {
-            
-            // TODO Add methods to call backend resource class (using AJAX)
-          
+            find: function (id){
+                return $http.get(url + "/" + id);
+            },
+            findAll: function (){
+                return $http.get(url);
+            },
+            delete: function (id){
+                return $http.delete(url + "/" + id);
+            },
+            create: function (product){
+                return $http.post(url, product); //Look into forms
+            },
+            update: function (id, product){
+                return $http.put(url + "/" + id, product);
+            },
+            findRange: function(first, count){
+                return $http.get(url + "/range?fst=" + first + "&count=" + count);
+            },
+            count: function(){
+                return $http.get(url + "/count");
+            }
         };
     }]);
 
